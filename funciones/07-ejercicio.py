b4f5b305a7716232303865cea156e72dde2509ba
Said <said.tecpa.juarez.04@gmail.com>
2025-03-26 21:04:47 -0600

    Update 07-ejercicio.py
    Solution to the exercise.

--- funciones/07-ejercicio.py
+++ funciones/07-ejercicio.py
@@ -1,5 +1,15 @@
-t = "Hola Mundo"
-for i in t:
-    resultado = 0
-    resultado += i
-    print(i)
+def palindromo(mensaje):
+    mensaje_sin_espacio = mensaje.lower().replace(" ", "")
+    texto_a = ""
+    texto_b = ""
+    for i in mensaje_sin_espacio:
+        texto_a += i
+    for h in range(len(mensaje_sin_espacio)-1, -1, -1):
+        contador = mensaje_sin_espacio[h]
+        texto_b += contador
+    if texto_a == texto_b:
+        print(f"El texto {mensaje} es un palindromo.")
+    else:
+        print(f"El texto {mensaje} no es un palindromo.")
+
+palindromo(mensaje="Abba")
