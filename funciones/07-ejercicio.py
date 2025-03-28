fc6cab3ca468f6b0a091ca97fe1da23a71a129e4
Said <said.tecpa.juarez.04@gmail.com>
2025-03-27 23:45:01 -0600

    Segunda solución.
    Se implemento una segunda solución al ejercicio. La solución 2 esta comentada para que no genere problemas al ejecutar la primera.

--- funciones/07-ejercicio.py
+++ funciones/07-ejercicio.py
@@ -12,4 +12,31 @@ def palindromo(mensaje):
     else:
         print(f"El texto {mensaje} no es un palindromo.")
 
-palindromo(mensaje="Abba")
+palindromo(mensaje="Reconocer")
+
+
+
+
+
+# Solución 2.
+# def no_space(texto):
+#     nuevo_texto = ""
+#     for char in texto:
+#         if char != " ":
+#             nuevo_texto += char
+#     return nuevo_texto
+
+# def reverse(texto):
+#     texto_al_reves = ""
+#     for char in texto:
+#         texto_al_reves = char + texto_al_reves
+#     return texto_al_reves
+
+# def es_palindromo(texto):
+#     texto = no_space(texto)
+#     texto_al_reves = reverse(texto)
+#     return texto.lower() == texto_al_reves.lower()
+
+# print(es_palindromo("amo la paloma"))
+# print(es_palindromo("Hola Mundo"))
+# print(es_palindromo("Reconocer"))
