d9c15385755bd0cb773c0700beeebe09ae2ba7a6
said-tj <said.tecpa.juarez.04@gmail.com>
2025-04-15 01:30:01 -0600

    app simple de calculadora

---
+++ apps-simples/calculadora.py
@@ -0,0 +1,34 @@
+# Datos de entrada.
+"""
+Solicitar al usuario que ingrese dos números
+cualesquiera.
+"""
+a_numero = float(input("Ingresa el primer número: "))
+b_numero = float(input("Ingresa el segundo número: "))
+
+
+# Procesamiento de datos.
+"""
+Solicitar al usuario que indique 
+que operación se va a realizar.
+"""
+mensaje_inicial = f"Ingresa que operación quieres hacer con el número {a_numero} y {b_numero}"
+mensaje_operaciones = f"suma, resta, multi, div"
+print(mensaje_inicial, mensaje_operaciones)
+
+operacion = input("Ingresa la operación: ")
+
+if operacion == "suma":
+    r_suma = a_numero + b_numero
+    print(r_suma)
+elif operacion == "resta":
+    r_resta = a_numero - b_numero
+    print(r_resta)
+elif operacion == "multi":
+    r_multi = a_numero * b_numero
+    print(r_multi)
+elif operacion == "div":
+    r_div = a_numero / b_numero
+    print(r_div)
+
+
